b6188e455d94abad632f78ca2406a7e1ff66bf2e
leilani-cardenas <72056787+leilani-cardenas@users.noreply.github.com>
2022-01-02 02:33:52 -0800

    Updated pages

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,9 +2,81 @@ import React, { Component } from "react";
 
 class Home extends Component {
     render(){
-        return(
-            <div>Hi, this is home.</div>
+        document.title="Home"
 
+        return(
+            <main>
+                <section className="py-5 text-center container">
+                    <div className="row py-lg-5">
+                        <div className="col-lg-6 col-md-8 mx-auto">
+                            <h1 className="fw-light">Book Collections</h1>
+                            <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+                        </div>
+                    </div>
+                </section>
+
+                <div className="album py-5 bg-light">
+                    <div className="container">
+                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+                            <div className="col">
+                                <div className="card shadow-sm">
+                                    <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                                    <div className="card-body">
+                                        <p className="card-text">This is the most recent book saved.</p>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <div className="btn-group">
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
+                                            </div>
+                                            <small className="text-muted">DD/MM/YYYY</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col">
+                                <div className="card shadow-sm">
+                                    <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                                    <div className="card-body">
+                                        <p className="card-text">This is the second most recent book saved.</p>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <div className="btn-group">
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
+                                            </div>
+                                            <small className="text-muted">DD/MM/YYYY</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                            <div className="col">
+                                <div className="card shadow-sm">
+                                    <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                                    <div className="card-body">
+                                        <p className="card-text">This is the third most recent book saved.</p>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <div className="btn-group">
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
+                                            </div>
+                                            <small className="text-muted">DD/MM/YYYY</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </main>
         );
     }
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,8 +2,40 @@ import React, { Component } from "react";
 
 class Login extends Component {
     render(){
+        document.title="Login"
+
         return(
-            <div>Hi this is login.</div>
+            <div className="container">
+                <div className="col-4">
+                    <body className="text-center">
+            
+                        <main className="form-signin">
+                            <form>
+                                <img className="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
+                                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+                                <div className="form-floating">
+                                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
+                                    <label htmlFor="floatingInput">Email address</label>
+                                </div>
+                                <div className="form-floating">
+                                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"></input>
+                                    <label htmlFor="floatingPassword">Password</label>
+                                </div>
+
+                                <div className="checkbox mb-3">
+                                    <label>
+                                        {/*<input type="checkbox" value="remember-me"> Remember me </input>*/} 
+                                    </label>
+                                </div>
+                                <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                                <p className="mt-5 mb-3 text-muted">© 2017-2021</p>
+                            </form>
+                        </main>
+
+                    </body>
+                </div>
+            </div>
         );
     }
 }
