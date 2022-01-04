e2bb91d40159f1d2e3f3853d8b88178f3449349b
leilani-cardenas <72056787+leilani-cardenas@users.noreply.github.com>
2022-01-03 18:30:15 -0800

    Save state for login and sign up

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,53 @@ import './login.css';
 
 class Login extends Component {
 
+    userData;
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    onChangeEmail(e) {
+        this.setState({ email: e.target.value })
+    }
+
+    onChangePassword(e) {
+        this.setState({ password: e.target.value})
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+
+    }
+
+    componentDidMount() {
+        this.userData = JSON.parse(localStorage.getItem('user'));
+
+        if (localStorage.getItem('user')) {
+            this.setState({
+                email: this.userData.email,
+                password: this.userData.password
+            })
+        } else {
+            this.setState({
+                email: '',
+                password: ''
+            })
+        }
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('user', JSON.stringify(nextState));
+    }
     
     render(){
         document.title="Login"
@@ -13,16 +60,16 @@ class Login extends Component {
                     <body className="text-center">
             
                         <main className="form-signin">
-                            <form>
+                            <form onSubmit={this.onSubmit}>
                                 <img className="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
                                 <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
                                 <div className="form-floating">
-                                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
+                                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" value={this.state.email} onChange={this.onChangeEmail}></input>
                                     <label htmlFor="floatingInput">Email address</label>
                                 </div>
                                 <div className="form-floating">
-                                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"></input>
+                                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" value={this.state.password} onChange={this.onChangePassword}></input>
                                     <label htmlFor="floatingPassword">Password</label>
                                 </div>
 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,6 +1,55 @@
 import React, { Component } from "react";
 
 class Signup extends Component {
+
+    userData;
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    onChangeEmail(e) {
+        this.setState({ email: e.target.value })
+    }
+
+    onChangePassword(e) {
+        this.setState({ password: e.target.value})
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+
+    }
+
+    componentDidMount() {
+        this.userData = JSON.parse(localStorage.getItem('user'));
+
+        if (localStorage.getItem('user')) {
+            this.setState({
+                email: this.userData.email,
+                password: this.userData.password
+            })
+        } else {
+            this.setState({
+                email: '',
+                password: ''
+            })
+        }
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('user', JSON.stringify(nextState));
+    }
+
     render(){
         document.title="Sign Up"
 
@@ -15,31 +64,18 @@ class Signup extends Component {
                         </div>
 
                         <div className="modal-body p-5 pt-0">
-                            <form className="">
-                            <div className="form-floating mb-3">
-                                <input type="email" className="form-control rounded-4" id="floatingInput" placeholder="name@example.com"></input>
-                                <label for="floatingInput">Email address</label>
-                            </div>
-                            <div className="form-floating mb-3">
-                                <input type="password" className="form-control rounded-4" id="floatingPassword" placeholder="Password"></input>
-                                <label for="floatingPassword">Password</label>
-                            </div>
-                            <button className="w-100 mb-2 btn btn-lg rounded-4 btn-primary" type="submit">Sign up</button>
-                            <small className="text-muted">By clicking Sign up, you agree to the terms of use.</small>
-                            <hr className="my-4"></hr>
-                            <h2 className="fs-5 fw-bold mb-3">Or use a third-party</h2>
-                            <button className="w-100 py-2 mb-2 btn btn-outline-dark rounded-4" type="submit">
-                                <svg className="bi me-1" width="16" height="16"></svg>
-                                Sign up with Twitter
-                            </button>
-                            <button className="w-100 py-2 mb-2 btn btn-outline-primary rounded-4" type="submit">
-                                <svg className="bi me-1" width="16" height="16"></svg>
-                                Sign up with Facebook
-                            </button>
-                            <button className="w-100 py-2 mb-2 btn btn-outline-secondary rounded-4" type="submit">
-                                <svg className="bi me-1" width="16" height="16"></svg>
-                                Sign up with GitHub
-                            </button>
+                            <form className="" onSubmit={this.onSubmit}>
+                                <div className="form-floating mb-3">
+                                    <input type="email" className="form-control rounded-4" id="floatingInput" placeholder="name@example.com" value={this.state.email} onChange={this.onChangeEmail}></input>
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input type="password" className="form-control rounded-4" id="floatingPassword" placeholder="Password" value={this.state.password} onChange={this.onChangePassword}></input>
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+                                <button className="w-100 mb-2 btn btn-lg rounded-4 btn-primary" type="submit">Sign up</button>
+                                <small className="text-muted">By clicking Sign up, you agree to the terms of use.</small>
+                                
                             </form>
                         </div>
                     </div>
